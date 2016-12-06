bb6d4bf4dbd4d67834acc8d9b3cdcca0645b5fdf
leongatys <leon.gatys@bethgelab.org>
2016-12-06 14:00:34 +0100

    fixed path to execute from main dir

--- models/download_leon_models.sh
+++ models/download_leon_models.sh
@@ -1,7 +1,7 @@
 BASE_URL="http://bethgelab.org/media/uploads/stylecontrol/feedforwardmodels/"
 
-mkdir -p trained
-cd trained
+mkdir -p models/trained
+cd models/trained
 curl -O "$BASE_URL/candy_lum_256-lum.t7"
 curl -O "$BASE_URL/candy_lum_256-lum.json"
 curl -O "$BASE_URL/candy_over_feathers_256_guidance_sw_5.0_slim.t7"
